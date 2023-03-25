b4fc006e330a52f71790750f4bceba11c23f2b2a
MominAbdurRaheman <127850737+MominAbdurRaheman@users.noreply.github.com>
2023-03-25 11:29:23 +0530

    Create f.py

---
+++ f.py
@@ -0,0 +1,29 @@
+[11:23, 25/03/2023] Momin Abdur Raheman: # Data Science Tools and Ecosystem
+[11:24, 25/03/2023] Momin Abdur Raheman: In this notebook, Data Science Tools and Ecosystem are summarized
+[11:24, 25/03/2023] Momin Abdur Raheman: Some of the popular languages that Data Scientists use are:
+1. Python
+2. R
+3. Java
+Some of the commonly used libraries used by Data Scientists include:
+1. NumPy
+2. Panda
+3.  matplotlib
+[11:24, 25/03/2023] Momin Abdur Raheman: Data Science Tool
+1. Matlab
+2. SAS
+3. BigML
+[11:25, 25/03/2023] Momin Abdur Raheman: ### Below are a few examples of evaluating arithmetic expressions in Python
+2*6
+2/3
+2+6
+3-1
+[11:25, 25/03/2023] Momin Abdur Raheman: This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+[11:25, 25/03/2023] Momin Abdur Raheman: This will convert 200 minutes to hours by diving by 60
+hours =200/60
+[11:25, 25/03/2023] Momin Abdur Raheman: Objectives:
+list of most popular libraries for data science
+list of most popular tool for data science
+list of most popular platforms for data science
+[11:26, 25/03/2023] Momin Abdur Raheman: ## Author
+Momin Mohammad Abdur Raheman
